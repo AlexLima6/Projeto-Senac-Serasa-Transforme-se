765c53871011f57f42351d2a8de40be8be4782a2
ALEX SANTOS LIMA <alex.slima14@senacsp.edu.br>
2025-04-30 13:27:10 -0300

    Adicionado a tela de estoque

--- ProjetoIntegrador/PainelEstoque.Designer.cs
+++ ProjetoIntegrador/PainelEstoque.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PainelEstoque";
+            maskedTextBox1 = new MaskedTextBox();
+            lblEditarproduto = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(589, 287);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(100, 23);
+            maskedTextBox1.TabIndex = 0;
+            // 
+            // lblEditarproduto
+            // 
+            lblEditarproduto.AutoSize = true;
+            lblEditarproduto.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            lblEditarproduto.Location = new Point(26, 24);
+            lblEditarproduto.Name = "lblEditarproduto";
+            lblEditarproduto.Size = new Size(139, 21);
+            lblEditarproduto.TabIndex = 1;
+            lblEditarproduto.Text = "Adicionar Produto:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label2.Location = new Point(26, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(138, 21);
+            label2.TabIndex = 2;
+            label2.Text = "Nome do Produto:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label3.Location = new Point(26, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Descrição:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label4.Location = new Point(342, 354);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 21);
+            label4.TabIndex = 4;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            label5.Location = new Point(447, 344);
+            label5.Name = "label5";
+            label5.Size = new Size(52, 21);
+            label5.TabIndex = 5;
+            label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 99);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(459, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(26, 219);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // PainelEstoque
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCoral;
+            ClientSize = new Size(511, 448);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lblEditarproduto);
+            Controls.Add(maskedTextBox1);
+            Name = "PainelEstoque";
+            Text = "PainelEstoque";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MaskedTextBox maskedTextBox1;
+        private Label lblEditarproduto;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
